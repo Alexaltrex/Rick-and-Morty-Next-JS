71b7de32bef8d34479fe5846a83c3304cc6bd11c
AlexAltrex <alexaltrex@mail.ru>
2022-04-10 15:20:05 +0300

    5

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -7,14 +7,10 @@ export default function Custom404() {
     return (
         <div className={style.custom404}>
             <div className={style.content}>
-                <div className={style.imgWrapper}>
-                    <Image src={src404}
-                           alt="404"
-                           placeholder="blur"
-                        //layout="fill"
-                        //objectFit="cover"
-                    />
-                </div>
+                <Image src={src404}
+                       width={200}
+                       height={171}
+                />
 
                 <h1>Page Not Found</h1>
                 <Link href="/"><a>Back to home</a></Link>
